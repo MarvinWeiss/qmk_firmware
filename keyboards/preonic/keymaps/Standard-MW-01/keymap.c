b8e56eba790ce83db6e57800a04855345f12f7a2
Marvin Weiss <marvinweiss@Marvins-iMac.local>
2020-11-08 11:44:39 +0100

    Added layout description to readme and added second cmd key

--- keyboards/preonic/keymaps/Standard-MW-01/keymap.c
+++ keyboards/preonic/keymaps/Standard-MW-01/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,  DE_P,    DE_UE,
   KC_TAB,  DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,  DE_OE,   DE_AE,
   KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,DE_MINS, KC_ENT,
-  KC_LCTL, KC_LALT,     KC_LGUI,     BL_OFF,     LOWER,       KC_SPC,      KC_SPC,      RAISE,       KC_LEFT,     KC_DOWN,   KC_UP,       KC_RGHT
+  KC_LCTL, KC_LALT,     KC_LGUI,     KC_RGUI,     LOWER,       KC_SPC,      KC_SPC,      RAISE,       KC_LEFT,     KC_DOWN,   KC_UP,       KC_RGHT
 ),
 
 /* Colemak
